cb6946a19c924589ca47775729aef83a4b43f462
Rui <mengrui930@gmail.com>
2020-03-21 23:05:26 -0700

    checked-box

--- src/App.js
+++ src/App.js
@@ -8,12 +8,26 @@ class App extends Component{
         super();
         this.state = {
             todos: todosData
-        }
+        };
+        this.handleChange = this.handleChange.bind(this)
+    }
+
+    handleChange(id) {
+        this.setState(prevState => {
+            const updatedTodos = prevState.todos.map(todo => {
+                if (todo.id === id) {
+                    todo.completed = !todo.completed
+                }
+                return todo
+            });
+            return {
+                todos: updatedTodos
+            }
+        })
     }
 
     render() {
-        const todoItem = this.state.todos.map(item => <TodoItem key={item.id} item={item}/>);
-
+        const todoItem = this.state.todos.map(item => <TodoItem key={item.id} item={item} handleChange={this.handleChange}/>);
         return (
             <div className="todo-list">
                 {todoItem}
